c0e3fe12aff0982ef92b2833422b1c396408bc0e
MaestroVLG <las-vegas1945@yandex.ru>
2024-04-24 23:35:14 +0300

    Initial commit

---
+++ goingtodream/homewoek2.py
@@ -0,0 +1,8 @@
+name = "Kirill"
+print(name)
+age = 25
+print(age)
+age = age + 1
+print(age)
+is_student = True
+print(is_student)
---
+++ goingtodream/homework4.py
@@ -0,0 +1,6 @@
+immutable_var = (1, 2, "a", "b")
+print(immutable_var)
+mutable_list = ([1, 2, 3], 0)
+print(mutable_list)
+mutable_list[0][2] = 400
+print(mutable_list)
