137a8ee9cf62d199345f88085a766509ff216462
MaestroVLG <las-vegas1945@yandex.ru>
2024-04-29 00:31:49 +0300

    внём правки во второй файл, странно конечно всё это работает

--- goingtodream/homework3.py
+++ goingtodream/homework3.py
@@ -1,6 +1,7 @@
-name = "my_string"
-print(name.lower())
-print(name.upper())
-print(name.replace("my_string" , "my_string"))
-print(name[0])
-print(name[-1])
+my_string = input ("Как дела на заводе, пацаны?, ")
+print("Длина строки:", len(my_string))
+print(my_string.lower())
+print(my_string.upper())
+print(my_string.replace(" " , ""))
+print(my_string[0])
+print(my_string[-1])
