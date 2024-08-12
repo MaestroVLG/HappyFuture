549bd4a8e40c4a904cd870d768e09df8fa81983e
MaestroVLG <las-vegas1945@yandex.ru>
2024-08-12 16:26:35 +0300

    .

--- module_9_6(Генераторы).py
+++ module_9_6(Генераторы).py
@@ -1,17 +1,35 @@
+# def all_variants(text):
+#     n = len(text)
+#
+#     for i in range(n):
+#         for j in range(n):
+#             for j in range(i + 1, n + 1):
+#                 yield text[i:j]
+#
+#
+# a = all_variants("abc")
+# for i in a:
+#     print(i)
+#
+# lst = [1, 2, 3]
+# a = all_variants(lst, 2)
+# for i in a:
+#     print(i)
+
+
 def all_variants(text):
+
     n = len(text)
 
-    for i in range(n):
+    for i in range(1 << n):
+        subsequence = ""
         for j in range(n):
-            for j in range(i + 1, n + 1):
-                yield text[i:j]
+            if (i & (1 << j)):
+                subsequence += text[j]
+        yield subsequence
 
 
-a = all_variants("abc")
-for i in a:
-    print(i)
-
-lst = [1, 2, 3]
-a = all_variants(lst, 2)
-for i in a:
-    print(i)
+if __name__ == "__main__":
+    a = all_variants("abc")
+    for i in a:
+        print(i)
