e202392f95556027282cd074bd6d57d273309e82
MaestroVLG <las-vegas1945@yandex.ru>
2024-08-26 22:18:50 +0300

    .

--- module_10_5(Многопроцессное программирование).py
+++ module_10_5(Многопроцессное программирование).py
@@ -15,19 +15,21 @@ def read_info(filename):
             line = f.readline()
 
 if __name__ == '__main__':
-    filenames = [f'./Files/file {number}.txt' for number in range(1, 4)]
+    filenames = [f'./Files/file {number}.txt' for number in range(1, 5)]
 
 #     #Линейный вызов
-# start = time.time()
-# for filename in filenames:
-#     with open(filename, 'r') as f:
-#            read_info(filename)
-# end = time.time()
-# print(end - start)
+    start = time.time()
+    for filename in filenames:
+        with open(filename, 'r') as f:
+           read_info(filename)
+    end = time.time()
+    print(end - start)
 
 # Многопроцессорный вызов
     start = time.time()
-    with Pool() as pool:
+    with Pool(processes=4) as pool:
         pool.map(read_info, filenames)
     end = time.time()
     print(end - start)
+
+
