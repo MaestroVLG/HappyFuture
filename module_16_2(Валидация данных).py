db2441af5b3611f5589b9617907c6a588fff1caa
MaestroVLG <las-vegas1945@yandex.ru>
2024-10-28 00:45:53 +0300

    основы FastAPI и маршрутизация.

--- module_16_2(Валидация данных).py
+++ module_16_2(Валидация данных).py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -11,14 +12,15 @@ async def admin() -> dict:
     return {"message": "Вы вошли как администратор"}
 
 @app.get('/user/{user_id}')
-def user(user_id: str):
+def user(user_id: Annotated[int, Path(title='User ID', description='Enter User ID', gt=0, le=100)]):
     return {'mes': f'Вы вошли как пользователь №{user_id}'}
 
-@app.get("/user")
-async def info(username: str = 'Federer', age: int = 35) -> dict:
+@app.get("/user/{username}/{age}")
+async def info(username: Annotated[str, Path(title="Username", description="Enter username", min_length=5, max_length=20)],
+               age: Annotated[int, Path(title="Age", description="Enter age", ge=18, le=120)]) -> dict:
     return {"message":f'Информация о пользователе. Имя:{username}, Возраст:{age}'}
 
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='127.0.0.1', port=8010)
+    uvicorn.run(app, host='127.0.0.1', port=8015)
