6e4cddcaf374ce1e7fbc78f0fc4b629cdb577ea3
MaestroVLG <las-vegas1945@yandex.ru>
2024-10-29 02:23:23 +0300

    Работа с моделью кода Pydantic

---
+++ module_16_4(Модели данных Pydantic).py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
+users: List[User] = []
+
+
+# CRUD операции(функции, главное понять)
+
+@app.get("/")
+async def welcome() -> dict:
+    return {"message": "Главная страница"}
+
+@app.get("/user/admin")
+async def admin() -> dict:
+    return {"message": "Вы вошли как администратор"}
+
+@app.get("/users", response_model=List[User])
+async def get_users() -> List[User]:
+    return users
+
+@app.post("/user/{username}/{age}", response_model=User)
+async def create_user(username: str, age: int) -> User:
+    new_id = 1 if not users else users[-1].id + 1
+    new_user = User(id=new_id, username=username, age=age)
+    users.append(new_user)
+    return new_user
+
+@app.put("/user/{user_id}/{username}/{age}", response_model=User)
+async def update_user(user_id: int, username: str, age: int) -> User:
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
+
+@app.delete("/user/{user_id}", response_model=User)
+async def delete_user(user_id: int) -> User:
+    for user in users:
+        if user.id == user_id:
+            users.remove(user)
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
+
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='127.0.0.1', port=8015)
